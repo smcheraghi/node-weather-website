10a2fe4a8499837952a27860277b5c7cf2ffa51b
seyed mohammad cheraghi <35502397+smcheraghi@users.noreply.github.com>
2021-01-17 01:48:24 -0500

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+console.log('Client side of javascrypt file is loaded')
+
+// fetch('http://localhost:3000/weather?address=boston').then((res) => {
+//     res.json().then((data) => {
+//         if (data.error){
+//             console.log(data.error)
+//         } else {
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#msg-1')
+const messageTwo = document.querySelector('#msg-2')
+
+// messageOne.textContent = 'haha'
+
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    console.log(search.value)
+    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+    res.json().then((data) => {
+        if (data.error){
+            console.log(data.error)
+            messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            console.log(data.location)                          // show in browser developer consol
+            console.log(data.forecast)
+        }
+    })
+})
+})
---
+++ src/app.js
@@ -0,0 +1,97 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = 3000
+
+//Define path for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Set handlebars and view location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)              // To change views directory to mine
+hbs.registerPartials(partialsPath)
+
+// Setup static dir to serve
+app.use(express.static(publicDirectoryPath))
+
+
+// app.get('/', (req, res) => {
+//     // res.send('Hello, This is the home page')
+// })
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Mohammad'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Mohammad'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address){
+        return res.send({
+            error: 'You must provide a address term'
+        })
+
+    }
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {                            //http://localhost:3000/products?search=gta&rating=12
+    if (!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query)
+    res.send({
+        product: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+
+
+console.log(__dirname)
+console.log(path.join(__dirname, '..'))
+console.log(path.join(__dirname, '../..'))
+console.log(path.join(__dirname, '../public'))
